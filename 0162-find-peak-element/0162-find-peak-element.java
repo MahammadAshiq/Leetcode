2c5331e4f53e8cc14c4ac3091db58a85a2d43378
MahammadAshiq <mahammadaashiq1@gmail.com>
2026-07-23 15:36:55 +0530

    Time: 0 ms (100%), Space: 44.4 MB (5.41%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max=nums[0];
+        int idx=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(max<nums[i])
+            {
+                max= nums[i];
+                idx=i;
+            }
+        }
+        return idx;
+    }
+}
